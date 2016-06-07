839178a7210719cd879a064d298111da2b26f566
kawasaki-atsushi <kawasaki8910@gmail.com>
2016-06-07 18:35:00 +0900

    add read me

--- lib/world_postal_codes.rb
+++ lib/world_postal_codes.rb
@@ -6,22 +6,5 @@ module WorldPostalCodes
   DATA_DIR = File.dirname(__FILE__) + '/../data/japan.yml'
   ZIP_URL = "http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip"
   extend WorldPostalCodes::Identify
-
-  # class << self
-  #   def identify(code)
-  #     db[code] || {}
-  #   end
-
-  #   def db
-  #     @db ||= begin
-  #       YAML.load(File.open(DATA_DIR))
-  #     end
-  #   end
-
-  #   def load
-  #     db
-  #   end
-  # end
-
 end
 require 'world_postal_codes/railtie' if defined?(Rails)
